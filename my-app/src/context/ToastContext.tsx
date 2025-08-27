e60937a7aa11ec06458f521687ecabf05e98ad6f
Tyler Floss <tylerfloss@gmail.com>
2025-08-27 13:47:29 -0500

    switch to hot toast

--- my-app/src/context/ToastContext.tsx
+++ my-app/src/context/ToastContext.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useContext } from "react";
-import { Toast, ToastContainer } from "react-bootstrap";
+import React, { createContext, useContext } from "react";
+import { Toaster, toast } from "react-hot-toast";
 
 const ToastContext = createContext<any>(null);
 
@@ -8,38 +8,25 @@ export const useToast = () => useContext(ToastContext);
 export const ToastProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [show, setShow] = useState(false);
-  const [message, setMessage] = useState("");
-  const [variant, setVariant] = useState<"success" | "danger">("success");
-
-  const showToast = (msg: string, type: "success" | "danger" = "success") => {
-    setMessage(msg);
-    setVariant(type);
-    setShow(true);
+  const showToast = (msg: string, type: "success" | "error" = "success") => {
+    if (type === "success") toast.success(msg);
+    else toast.error(msg);
   };
 
   return (
     <ToastContext.Provider value={{ showToast }}>
       {children}
-      <ToastContainer
-        position="top-end"
-        className="p-3"
-        style={{ zIndex: 9999, color: "#90e6edff" }}
-      >
-        <Toast
-          show={show}
-          onClose={() => setShow(false)}
-          bg={variant}
-          delay={3000}
-          autohide
-        >
-          <Toast.Body
-            style={{ color: variant === "success" ? "#ffffffff" : "#ed8e8eff" }}
-          >
-            {message}
-          </Toast.Body>
-        </Toast>
-      </ToastContainer>
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+        }}
+      />
     </ToastContext.Provider>
   );
 };
